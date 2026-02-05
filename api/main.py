ff4f51d402d0153233fe3c64731fcacb509c3cbd
tristan_retali <tristan.retali@gmail.com>
2026-02-05 11:17:07 +0100

    Déut intégration spark dans les routes

--- api/main.py
+++ api/main.py
@@ -4,11 +4,22 @@ from spark_connexion import create_spark_session
 
 app = FastAPI(title="Ecotrack API")
 
-
-app.include_router(router)
+spark = None
 
 
 @app.on_event("startup")
 async def startup_event():
-    app.state.spark = create_spark_session()
+    global spark
+    spark = create_spark_session()
+    app.state.spark = spark
     print("Session Spark initialisée")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    if spark:
+        spark.stop()
+        print("Session Spark arrêtée")
+
+
+app.include_router(router)
--- api/routes.py
+++ api/routes.py
@@ -1,24 +1,28 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Request
+from pyspark.sql import SparkSession
 
 router = APIRouter()
 
 
+def get_spark_session(request: Request):
+    return request.app.state.spark
+
+
 @router.get("/sales_per_country")
-def read_root():
+def sales_per_country():
     return {"message": "vente par pays"}
 
 
 @router.get("/top_products")
-def read_root():
+def top_products():
     return {"message": "top produits"}
 
 
 @router.get("/return_products")
-def read_root():
+def return_products():
     return {"message": "produits retournés"}
 
 
 @router.get("/periodic")
-def read_root():
+def periodic():
     return {"message": "période"}
